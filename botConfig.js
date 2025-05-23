27ac0519fd9898e43a44f6c54aacf49360e8d810
CYBER-99 <126309291+CYBER-99@users.noreply.github.com>
2025-05-23 21:04:26 +0530

    Update botConfig.js

--- botConfig.js
+++ botConfig.js
@@ -1,8 +1,8 @@
 module.exports = {
-    token: "YOUR_TOKEN",
-    prefix: "PREFIX",
+    token: "MTM1Mzc3MzY0OTIzMjAwNzIzOQ.GdZiOk.qIq-NbhrNcpKtWIFAhEfApj0UnyO7M0-dPCsJg",
+    prefix: "cf!",
     admins: [
-        "PEOPLE WHO CAN USE ADD MONEY (IDS)"
+        "1222520832236978217"
 ],
     debug: true,
     countChannel: "countChannelID"
